c2d1d99bcbc9e4ab33e3fcecdc1cf6a6674c66eb
eldor <eldor@LAPTOP-895RKGSB>
2021-10-21 01:18:51 +0300

    Добавьте файлы проекта.

---
+++ kot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
